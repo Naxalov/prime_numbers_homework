293700c23e0bd691a2b26479baae2263824b8f0c
ZARIFMAC <uzbzarif@gmail.com>
2022-01-22 21:22:35 +0900

    is prime

--- is_prime.py
+++ is_prime.py
@@ -7,9 +7,10 @@ def is_prime(n):
     Returns:
         bool: True if prime, False if not.
     """
+    
     if n==1:
        return False 
-       
+
     for i in range(2,n):
         if n%i==0:
             return False
